fc47c9ff913acbf9252745913cd0a55f4af6e738
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-06-26 17:25:16 +0300

    feat: Created business entity User model and interface user repository

---
+++ Business/Entities/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Business.Entities;
+
+public class UserModel
+{
+    public int Id { get; set; }
+
+    public string Login { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string Pseudonym { get; set; }
+}
---
+++ Business/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Business.Entities;
+
+namespace Business.Repositories;
+
+public interface IUserRepository
+{
+    IEnumerable<UserModel> GetAll();
+    
+    UserModel GetById(int userId);  
+        
+    void Insert(UserModel userModel);
+        
+    void Delete(int userId);
+    
+    void Update(UserModel userModel);
+}
